ae9875bf2c8a7a475a6cfdeff7fa20d54d49bd52
Renuka <156447613+renukasalian@users.noreply.github.com>
2025-11-30 15:55:52 +0530

    Update loadSilver_proc.sql

--- scripts/silverLayer/loadSilver_proc.sql
+++ scripts/silverLayer/loadSilver_proc.sql
@@ -41,40 +41,39 @@ BEGIN
 		PRINT '>>Inserting Data Into: silver.crm_cust_info';
 		SET @start_time = GETDATE()
 		SET @batch_start_time = GETDATE()
-		INSERT INTO silver.crm_prd_cust_info(
-			prd_id,
-			cat_id,
-			prd_key,
-			prd_nm,
-			prd_cost,
-			prd_line,
-			prd_start_dt,
-			prd_end_dt
-			)
-
+		INSERT INTO silver.crm_cust_info(
+			cst_id,
+			cst_key,
+			cst_firstname,
+			cst_lastname,
+			cst_marital_status,
+			cst_gndr,
+			cst_create_date)
+		
 		SELECT 
-		prd_id,
-		REPLACE(SUBSTRING(prd_key,1,5),'-','_')AS cat_id,
-		SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key,
-		prd_nm,
-		ISNULL(prd_cost,0) AS prd_cost,
-		CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
-				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
-				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
-				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
-				ELSE 'n/a'
-		END AS prd_line,
-		CAST(prd_start_dt AS DATE) AS prd_start_dt,
-		cast(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
-		FROM bronze.crm_prd_cust_info;
-		SET @end_time = GETDATE()
-		PRINT '>>Load Duration - ' + CAST(DATEDIFF(second,@start_time,@end_time) AS VARCHAR) + ' seconds';
-		PRINT '>>>-----------';
-
-
-
-
-
+		cst_id,
+		cst_key,
+		TRIM(cst_firstname) AS cst_firstname,
+		TRIM(cst_lastname) AS cst_lastname,
+		
+		
+		CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+			WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+			ELSE 'n/a'
+		END cst_marital_status,
+		
+		CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+			WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+			WHEN cst_gndr IS NULL OR TRIM(cst_gndr)='' THEN 'n/a'
+			ELSE 'n/a'
+		END cst_gndr,
+		cst_create_date
+		
+		FROM ( 
+				SELECT *, ROW_NUMBER() OVER (PARTITION BY  cst_id ORDER BY cst_create_date DESC) as flag_last
+				FROM bronze.crm_cust_info
+				WHERE cst_id IS NOT NULL)t
+		WHERE flag_last = 1
 		
 
 		--Loading silver.crm_prd_cust_info
